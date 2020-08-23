91edd89d9abbb0c6cca9dbf3e0ad053f0f796543
黄鹏程 <359673734@qq.com>
2020-08-23 23:02:00 +0800

    test commit

--- src/main/java/com/huang/study/security/dao/UserRepository.java
+++ src/main/java/com/huang/study/security/dao/UserRepository.java
@@ -10,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UserRepository extends JpaRepository<SysUser, Long> {
     SysUser findByUserName(String userName);
+
+    void delete(long id);
 }
